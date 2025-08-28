4da02c7afd90b7c73c37d8171d1fcf963d744308
tusharjain8465 <tusharjain8465@gmail.com>
2025-08-28 16:09:38 +0530

    Update Client.java

--- src/main/java/com/example/wholesalesalesbackend/model/Client.java
+++ src/main/java/com/example/wholesalesalesbackend/model/Client.java
@@ -2,17 +2,17 @@ package com.example.wholesalesalesbackend.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -46,8 +46,8 @@ public class Client {
     private Long userId;
 
     // Make sure "client" matches the field name in SaleEntry
-    @OneToMany(mappedBy = "client")
-    @JsonManagedReference
+    @JsonIgnore
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
     private List<SaleEntry> sales;
 
 }
